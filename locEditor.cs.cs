06329e7fba71c45dab53ac2dba45cd01e0f74698
jsipola <juuso_sipola@hotmail.com>
2017-02-07 11:08:43 +0200

    moving objects during runtime
    downloading and moving objects during runtime

---
+++ locEditor.cs.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(gra))]
+public class locEditor : Editor {
+
+	public override void OnInspectorGUI(){
+		DrawDefaultInspector();
+		gra mygra = (gra) target;
+		if (GUILayout.Button("Make bot")) {
+			mygra.init();
+		}
+	}
+}
