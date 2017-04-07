45d9dc7ba68241717a93e913afa6a47daf09b80e
jsipola <juuso_sipola@hotmail.com>
2017-04-07 15:09:00 +0300

    backup

---
+++ gra - Copy.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+ 
+public class gra{
+	GameObject cube;
+    Thread receiveThread;
+    Thread iThread; 
+    Socket sock;
+    public string IP = "192.168.1.14";
+	public string DownloadLink = "192.168.1.84";
+    public int port = 54345;
+	public int port_c;
+	//int version = 1;
+
+    System.Text.ASCIIEncoding encode = new System.Text.ASCIIEncoding();
+    private IPEndPoint EP;
+    string data = string.Empty;
+    public string name = "";
+    Vector3 pos;
+    Vector3 rot;
+    //int done = 1;
+    MeshRenderer mr;
+
+	public bool inited = false;
+	
+	List<string> objs = new List<string>();
+	List<Thread> threadList = new List<Thread>();
+	List<Socket> socketList = new List<Socket>();
+	
+	Dictionary<string,Vector3> posit = new Dictionary<string,Vector3>();
+	Dictionary<string,Vector3> rots = new Dictionary<string,Vector3>();
+	
+	
+    public void Start(){
+		//done = 1;
+    }
+
+	/*private*/
+    public bool init(){
+		/////////////////////////////////////////////////////
+		// Initialises the object and makes the socket
+		// thread (activated when user presses "make bot" button)
+		/////////////////////////////////////////////////////
+		pos = new Vector3(0,0,0);
+		rot = new Vector3(0,0,0);
+		
+/*		if (GameObject.Find(name)) {
+			EditorUtility.DisplayDialog("Error","Object with the same name found, duplicates can't exist", "OK", "No");
+			return false;
+		}
+*/
+		if (objs.Contains(name)) {
+			objs.Remove(name);
+			posit.Remove(name); // position dict
+			rots.Remove(name);	// rotation dict
+		}
+
+		cube = downloadObj(); // download object during runtime and rename it to "name" variable
+		if (cube == null){ // if no object is found on disk or server is inaccessible
+			Debug.Log("Failure loading object");
+			//EditorUtility.DisplayDialog("Error","No such object found on Disk or remote Server", "OK", "No");
+			return false;
+		}
+
+		objs.Add(name); // add object to the list
+		posit.Add(name,pos); // position dict
+		rots.Add(name,rot);	// rotation dict
+
+		EP = new IPEndPoint(IPAddress.Parse(IP),port);
+		sock = new Socket(EP.AddressFamily, SocketType.Stream, ProtocolType.Tcp); // make tcp socket
+		socketList.Add(sock);
+		try {
+			sock.ReceiveTimeout = 4000; // if no msg is received from the server in 4 seconds, closes the socket
+			sock.Connect(EP); // TODO: if failed try later in socket_tick
+			sock.Send(Encoding.UTF8.GetBytes("l pos\n")); // send msg to tell server start sending pos 
+			Debug.Log("CREATING Socket Thread");
+			receiveThread = new Thread(() => socket_thread(sock)); // makes thread for receiving data
+			receiveThread.IsBackground = true;
+			receiveThread.Start();
+			threadList.Add(receiveThread);
+			Debug.Log("Started");
+			return true;
+		} catch (Exception e) {
+			sock.Close();
+			Debug.Log("Problem with the socket: "+ e.ToString());
+			return false;
+		}
+	}
+	
+    void Update(){
+		moveObject();
+    }
+
+	     
+    private void socket_thread(Socket soc){
+		////////////////////////////
+		//Receives the location data from the server and saves them to dictionaries
+		////////////////////////////
+        try {
+			string[] buffer = "pos 0 0 0 0 0 0".Split();
+			while (Thread.CurrentThread.IsAlive){
+				byte[] rec = new byte[100];
+				soc.Receive(rec);
+				data = encode.GetString(rec);
+				Debug.Log("REC:"+data);
+				buffer = data.Split();
+				string[] model = buffer[0].Split(new Char[] {'.'}); // get the object which the server want to move
+				if (model[1] == "pos"){
+					pos[0] = float.Parse(buffer[1]);
+					pos[1] = float.Parse(buffer[3]); // Y up in unity
+					pos[2] = float.Parse(buffer[2]);
+					rot[0] = float.Parse(buffer[4]);
+					rot[1] = -float.Parse(buffer[6]); // horizontal rotation
+					rot[2] = float.Parse(buffer[5]);
+					if (objs.Contains(model[0])){
+						posit[model[0]] = pos;
+						rots[model[0]] = rot;
+					}
+				}
+				Thread.Sleep(10);
+			}  
+        } catch(Exception e) {
+			//sock = null;
+			Debug.Log("EEEE"+e);
+		}
+    }
+	 
+	private GameObject downloadObj(){
+		//////////////////////////////
+		//// Checks if the object is located on directory
+		//// if not downloads from the server
+		/////////////////////////////
+		string path = Directory.GetCurrentDirectory();
+		//string path = System.IO.Path.GetDirectoryName(Application.dataPath);
+		bool found = false;
+		bool server = true;
+		if (File.Exists(path+"/"+name+".obj")){
+			found = true;
+			Debug.Log("OBJ file found");
+		}
+		if (!found){
+			using (var client = new System.Net.WebClient())
+			{
+				try { // downloads object and mtl files from a HTTP server at address "DownloadLink"
+					Debug.Log("Downloading files");
+					client.DownloadFile("http://"+DownloadLink+":8000/"+name+".obj", path+"/"+name+".obj");
+					client.DownloadFile("http://"+DownloadLink+":8000/"+name+".mtl", path+"/"+name+".mtl");
+				} catch (Exception e) {
+					Debug.Log("Problem downloading obj: "+ e.ToString());
+					server = false;
+				}
+			}
+		}
+		path = System.IO.Path.Combine(@path,name+".obj");
+		if ((found) || (server)) { // if the object is either downloaded or already on disk 
+			GameObject cuube = OBJLoader.LoadOBJFile(path);	
+			cuube.name = name; // rename the object
+			Debug.Log("Object loaded");
+			return cuube;
+		} else {
+			Debug.Log("File not found on disk and download failed");
+			GameObject game = null;
+			return game;
+		}
+	}
+
+	public void moveObject(){
+		///////////////////////////
+		// Move objects in the scene
+		///////////////////////////
+		Vector3 pos_vec;
+		Vector3 rot_vec;
+		try {
+			foreach(var entry in objs) { // rotates the objects of the scene
+				pos_vec = posit[entry];
+				rot_vec = rots[entry];
+				GameObject obj = GameObject.Find(entry);
+				obj.transform.position = pos_vec;
+				obj.transform.eulerAngles = rot_vec;
+			}
+		} catch(Exception e) {
+			//Debug.Log("Problem moving object: "+ e.ToString());
+		}
+		
+	}
+	
+	public void endProcesses(){
+		/////////////////////////////////////////////////
+		///// Ending procedures when not used as a script
+		/////////////////////////////////////////////////
+		try {
+			foreach(var thread in threadList) { // rotates the objects of the scene
+				thread.Abort();
+			}
+		} catch(Exception e) {
+			Debug.Log("Problem ending threads: "+ e.ToString());
+		}
+		try {
+			foreach(var socket in socketList) { // rotates the objects of the scene
+				if (socket!=null) socket.Close(); 
+			}
+		} catch(Exception e) {
+			Debug.Log("Problem ending sockets: "+ e.ToString());
+		}
+		Debug.Log("Stop");   
+	}
+	
+	
+     public void OnApplicationQuit(){
+		/////////////////////////////
+		//// Application end procedures
+		/////////////////////////////
+		try {
+			foreach(var thread in threadList) { // rotates the objects of the scene
+				thread.Abort();
+			}
+		} catch(Exception e) {
+			Debug.Log("Problem ending threads: "+ e.ToString());
+		}
+		try {
+			foreach(var socket in socketList) { // rotates the objects of the scene
+				if (socket!=null) socket.Close(); 
+			}
+		} catch(Exception e) {
+			Debug.Log("Problem ending sockets: "+ e.ToString());
+		}
+		Debug.Log("Stop"); 
+     }    
+}
---
+++ handler.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class handler : MonoBehaviour {
+
+	UdpClient listener;
+	string server_r =  "192.168.1.84";
+	string server_c;
+	string server_o;
+	string IP;
+	//string name;
+	Socket soc_1;
+	int port_r = 54545;
+	int port;
+	bool done = true;
+	bool makeobject = false;
+	gra mygra = null;
+	System.Text.ASCIIEncoding encode = new System.Text.ASCIIEncoding();
+
+
+	// Use this for initialization
+	void Start() {
+		// start  thread for receiving data
+		Thread recThread = new Thread(() => commslisten()); 
+		recThread.IsBackground = true;
+		recThread.Start();
+		//checkGra();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (mygra != null) {
+			if (makeobject) {
+				mygra.init();
+				makeobject = false;
+/*			if (!mygra.inited) {
+				mygra.init();
+				mygra.inited = true;
+*/			} else {
+				mygra.moveObject();
+			}
+		}
+	}
+	
+	void commslisten()
+	{
+		//////////////////////////////////////////////////////
+		//////Main function which waits for a msg from an user
+		//////////////////////////////////////////////////////
+		IPEndPoint EP;
+		listener = new UdpClient(8000);
+		EP = new IPEndPoint(IPAddress.Any,8000);
+		byte[] rec = new byte[100];
+		string[] buffer;
+		string data;
+		//Socket sok = new Socket(EP.AddressFamily, SocketType.Stream, ProtocolType.Udp);
+		
+		Thread.Sleep(1000);
+		while (done)
+		{
+			try {
+				print("WAITING FOR INCOMING MESSAGES");
+				rec = listener.Receive(ref EP); // blocking call !!!
+				data = encode.GetString(rec);
+				print("REC:"+data);
+				buffer = data.Split();
+				if (buffer[0] == "make")
+				{
+					init(buffer[1]);
+				}
+				print("CER:");
+				Thread.Sleep(10);
+			} catch (Exception e) {
+				print("MAIN::"+e.ToString());
+			}
+		}
+	}
+
+	void init(string name)
+	{
+		///////////////////////////////////////////////////////
+		////// Initialize the gra object and give the arguments
+		///////////////////////////////////////////////////////
+		soc_1 = makeSocket(server_r, port_r);
+		// object and coordinate servers
+		// in format ip:port
+		server_o = getUrl(name+".object",soc_1);
+		if (server_o == null) {
+			return;
+		}
+		server_c = getUrl(name+".pos",soc_1);
+		if (server_c == null) {
+			return;
+		}
+		// split as "ip" and "port"
+		string[] server_oo = server_o.Split(new Char[] {':'});
+		print(server_oo[0]+ " " + server_oo[1]);
+		string[] server_cc = server_c.Split(new Char[] {':'});
+		print(server_cc[0]+ " " + server_cc[1]);
+		
+		if (mygra == null){
+			mygra = new gra();
+		}
+		// ip of the object server
+		mygra.DownloadLink = server_oo[0];
+		//DownloadLink = server_oo[0];
+		
+		//mygra.port = Int32.Parse(server_oo[1]);
+		//port = Int32.Parse(server_oo[1]);
+		
+		// ip of the coordinate server
+		mygra.IP = server_cc[0];
+		//IP = server_cc[0];
+		// port of the coords server
+		//mygra.port = server_cc[1];
+		
+		//string name1 = "greencube";
+		mygra.name = name;
+		makeobject = true;
+	}
+
+	Socket makeSocket(string serv, int port){
+		////////////////////////////////////////////////////////
+		////// Makes a socket for the specified address and port
+		////////////////////////////////////////////////////////
+		try {
+			IPEndPoint EP;
+			EP = new IPEndPoint(IPAddress.Parse(serv),port);
+			Socket sok = new Socket(EP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+			sok.Connect(EP);
+			return sok;
+		} catch (Exception e) {
+			print("SOCKET::"+ e.ToString());
+			return null;
+		}
+	}
+
+	string getUrl(string msg,Socket soc){
+		////////////////////////////////////////////////////////
+		/////// Sends a msg to the socket and reads the response
+		////////////////////////////////////////////////////////
+		try {
+			soc.Send(Encoding.UTF8.GetBytes(msg+"\n"));
+			byte[] rec = new byte[100];
+			soc.Receive(rec);
+			string data = encode.GetString(rec);
+			print("DATA: "+ data);
+			return data;
+		} catch (Exception e) {
+			print("URL::"+ e.ToString());
+			return null;
+		}
+	}
+
+	public void OnApplicationQuit(){
+		///////////////////////////////
+		//// Application end procedures
+		///////////////////////////////
+		try 
+		{
+			mygra.endProcesses();
+			done = false;
+			listener.Close();
+			soc_1.Close();
+		} catch(Exception e) 
+		{
+			print("Problem closing sockets: "+ e.ToString());
+		}
+		print("Stop"); 
+    }
+}
